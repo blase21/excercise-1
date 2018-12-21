232e010aefb2422a4dac80dd48b2ea04d51eea17
POD1-4 <imshiny808@gmail.com>
2018-12-21 10:12:16 -1000

    finish

--- blaze/arrays and objects/excerise-1.js
+++ blaze/arrays and objects/excerise-1.js
@@ -1,4 +1,21 @@
-var colors = ["black", "white", "green", "purple", "red"]
-console.log(0)
-console.log(2)
+var colors = ["green", "blue", "red", "yellow", "grey"];
 
+console.log(colors[0]);
+console.log(colors[2]);
+
+colors[4] = "black";
+var fourthColor = colors[3];
+colors.push("purple");
+
+colors.unshift("white");
+
+console.log(colors.length);
+colors.pop();
+console.log(colors.length);
+
+var ben;
+var shapiro = " ";
+for (ben = 0; ben < colors.length; ben++){
+    shapiro += colors[ben] + " ";
+}
+console.log(shapiro);
